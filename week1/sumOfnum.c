91edecbe3b9298c7242eba60ef228ba5bb55b8b2
Ghost-Maxx <144104454+Ghost-Maxx@users.noreply.github.com>
2024-09-08 13:18:57 +0530

    Create sumOfnum.c

---
+++ week1/sumOfnum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a, b, sum=0;
+    //taking input from user 
+    printf("Enter the Numbers \n");
+    scanf("%d%d", &a, &b);
+    sum = a+b;
+    printf("The sum is %d", sum);
+
+    return 0;
+}
